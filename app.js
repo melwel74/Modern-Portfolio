3269938bdc94cd23c5bc5f6c35edad4f61dead2a
Melvin Welton <nivlem65@gmail.com>
2022-08-01 16:23:13 -0500

    merge

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-var commandLineArgs = process.argv;
-console.log(commandLineArgs);
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+
+var playerHealth = 10;
+
+if(playerHealth === 0) {
+    console.log("This will not run");
+}
+else{
+    console.log("This will run instead");
+}
