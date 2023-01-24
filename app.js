6f44198f8e770b6b288700a5acddd92ad6a8bd36
Melvin Welton <nivlem65@gmail.com>
2023-01-24 11:19:00 -0600

    wrong js

--- app.js
+++ app.js
@@ -2,11 +2,3 @@
 // console.log(commandLineArgs);
 
 
-var playerHealth = 10;
-
-if(playerHealth === 0) {
-    console.log("This will not run");
-}
-else{
-    console.log("This will run instead");
-}
